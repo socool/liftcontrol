dc69fbf055cf8161563ea7d9143710fc34240612
Kamolsit Mongkolsrisawat <socool@Kamolsits-MacBook-Pro.local>
2018-08-13 10:45:55 +0700

    initialize project

---
+++ src/main/java/com/liftcontrol/liftcontrol/LiftcontrolApplication.java
@@ -0,0 +1,12 @@
+package com.liftcontrol.liftcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftcontrolApplication.class, args);
+	}
+}
