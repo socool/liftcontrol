f0292f6deaf0f8d030c3b3ecb540440cbc014158
Kamolsit Mongkolsrisawat <socool@Kamolsits-MacBook-Pro.local>
2018-08-13 11:09:12 +0700

    fix place wrong test case

--- src/test/java/com/liftcontrol/liftcontrol/LiftControltest.java
+++ src/test/java/com/liftcontrol/liftcontrol/LiftControltest.java
@@ -8,15 +8,17 @@ public class LiftControltest {
     @Test
     public void testOverWeight() {
         LiftControl lift = new LiftControl(1000,10);
+        lift.setWeight(1001);
+        lift.operate();
+        assertEquals(LiftControl.CONTROL.STOP,lift.getCurrentStatus());
 
     }
 
     @Test
     public void testNotOverWeight() {
-        LiftControl lift = new LiftControl(1000,10);
-        lift.setWeight(1001);
-        lift.operate();
-        assertEquals(LiftControl.CONTROL.STOP,lift.getCurrentStatus());
+
     }
+
+
 
 }
